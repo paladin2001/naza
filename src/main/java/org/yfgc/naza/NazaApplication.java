67a695c18897d58b99e5af8ae0d15af94a3d72b5
paladin00 <paladin2001@hanmail.net>
2023-03-10 16:57:20 +0900

    Initial commit

---
+++ src/main/java/org/yfgc/naza/NazaApplication.java
@@ -0,0 +1,13 @@
+package org.yfgc.naza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NazaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yfgc/naza/NazaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yfgc.naza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
